6ec206c8ce56124017c503744dc97c50d310a6f9
Tony Hauptmann <65155653+thauptmann@users.noreply.github.com>
2025-01-03 17:11:29 +0100

    Fix a bug

--- mrs_analysis.sh
+++ mrs_analysis.sh
@@ -2,11 +2,14 @@ N_CV_REPEATS=10
 N_CV_SPLITS=5
 DROP=1
 
-python src/mrs_analysis.py --data_set_name gbs_gesis --n_cv_repeats $N_CV_REPEATS --n_cv_splits $N_CV_SPLITS --drop $DROP --load_previous_results
-python src/mrs_analysis.py --data_set_name gbs_allensbach --n_cv_repeats $N_CV_REPEATS --n_cv_splits $N_CV_SPLITS --drop $DROP --load_previous_results
-python src/mrs_analysis.py --data_set_name gbs_gesis --n_cv_repeats $N_CV_REPEATS --n_cv_splits $N_CV_SPLITS --drop $DROP --mrs_function random --load_previous_results
-python src/mrs_analysis.py --data_set_name gbs_allensbach --n_cv_repeats $N_CV_REPEATS --n_cv_splits $N_CV_SPLITS --drop $DROP --mrs_function random --load_previous_results
-
+python src/mrs_analysis.py --data_set_name gbs_gesis --n_cv_repeats $N_CV_REPEATS \
+    --n_cv_splits $N_CV_SPLITS --drop $DROP --load_previous_results
+python src/mrs_analysis.py --data_set_name gbs_allensbach --n_cv_repeats $N_CV_REPEATS \
+    --n_cv_splits $N_CV_SPLITS --drop $DROP --load_previous_results
+python src/mrs_analysis.py --data_set_name gbs_gesis --n_cv_repeats $N_CV_REPEATS \
+    --n_cv_splits $N_CV_SPLITS --drop $DROP --mrs_function random --load_previous_results
+python src/mrs_analysis.py --data_set_name gbs_allensbach --n_cv_repeats $N_CV_REPEATS \
+    --n_cv_splits $N_CV_SPLITS --drop $DROP --mrs_function random --load_previous_results
 
 DROP=5
 for MRS_FUNCTION in mrs_step random 
--- temperature_comparison.sh
+++ temperature_comparison.sh
@@ -2,12 +2,22 @@ N_CV_REPEATS=10
 N_CV_SPLITS=5
 DROP=5
 
+for SAMPLE_WEIGHTING_METHOD in fw-mrs-temperature-svm fw-mrs-temperature 
+do
+    for BIAS_TYPE in mean_difference
+    do
+        python src/weighting_experiment.py --dataset folktables_income  --sample_weighting_method $SAMPLE_WEIGHTING_METHOD  \
+        --bias_type $BIAS_TYPE --n_cv_repeats $N_CV_REPEATS --n_cv_splits $N_CV_SPLITS \
+        --experiment_name temperature_comparison --drop $DROP --bias_fraction 0.8
+    done
+done
+
 for SAMPLE_WEIGHTING_METHOD in fw-mrs-temperature fw-mrs-temperature-svm
 do
     for BIAS_TYPE in less_positive_class less_negative_class mean_difference
     do
         python src/weighting_experiment.py --dataset folktables_income  --sample_weighting_method $SAMPLE_WEIGHTING_METHOD  \
         --bias_type $BIAS_TYPE --n_cv_repeats $N_CV_REPEATS --n_cv_splits $N_CV_SPLITS \
-        --experiment_name temperature_comparison --drop $DROP --bias_fraction 0.1 &
+        --experiment_name temperature_comparison --drop $DROP --bias_fraction 0.1 
     done
 done

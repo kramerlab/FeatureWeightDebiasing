55720db95c3030ec779463e118c4b9150c276f22
Tony Hauptmann <65155653+thauptmann@users.noreply.github.com>
2025-03-19 11:56:37 +0100

    New slurm config

--- src/utils/parameter.py
+++ src/utils/parameter.py
@@ -6,10 +6,30 @@ def set_parameter(method_name, data_set_name=None, bias_type=None):
     if method_name in ("fw-mrs-temperature",):
         draw_with_feature_weights = True
         if data_set_name == "folktables_income" and bias_type == "less_positive_class":
-            temperatures = [10, 1.0, 0.05, 0.01, 0.0, -0.001, -0.01, -1, -10]
+            temperatures = [
+                10.0,
+                5.0,
+                1.0,
+                0.1,
+                0.5,
+                0.05,
+                0.01,
+                0.005,
+                0.001,
+                0.0,
+                -0.001,
+                -0.005,
+                -0.05,
+                -0.01,
+                -0.5,
+                -0.1,
+                -1,
+                -5.0,
+                -10.0,
+            ]
         else:
             temperatures = [0.1, 0.05, 0.01]
-        hyperparameter_list = [0.025, 0.01, 0.0]
+        hyperparameter_list = [0.025, 0.01, 0.001, 0.0]
         dropped_samples_val_dict = {temperature: [] for temperature in temperatures}
         auroc_val_dict = {temperature: [] for temperature in temperatures}
         auprc_val_dict = {temperature: [] for temperature in temperatures}

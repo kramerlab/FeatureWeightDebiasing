9bc8fd0905a30048821eb5b4af729c9254812257
Tony Hauptmann <65155653+thauptmann@users.noreply.github.com>
2025-09-18 14:44:08 +0200

    Adapt mrs analysis experiments

--- mrs_analysis.sh
+++ mrs_analysis.sh
@@ -2,27 +2,17 @@ N_CV_REPEATS=10
 N_CV_SPLITS=5
 DROP=1
 
-python src/mrs_analysis.py --data_set_name gbs_gesis --n_cv_repeats $N_CV_REPEATS \
-    --n_cv_splits $N_CV_SPLITS --drop $DROP --load_previous_results
 python src/mrs_analysis.py --data_set_name gbs_allensbach --n_cv_repeats $N_CV_REPEATS \
     --n_cv_splits $N_CV_SPLITS --drop $DROP --load_previous_results
-python src/mrs_analysis.py --data_set_name gbs_gesis --n_cv_repeats $N_CV_REPEATS \
-    --n_cv_splits $N_CV_SPLITS --drop $DROP --mrs_function random --load_previous_results
 python src/mrs_analysis.py --data_set_name gbs_allensbach --n_cv_repeats $N_CV_REPEATS \
     --n_cv_splits $N_CV_SPLITS --drop $DROP --mrs_function random --load_previous_results
 
 DROP=5
 for MRS_FUNCTION in mrs_step random 
 do
-    for BIAS_TYPE in less_positive_class less_negative_class none
+    for BIAS_TYPE in less_positive_class 
     do 
         python src/mrs_analysis.py --data_set_name folktables_income --bias_type $BIAS_TYPE --n_cv_repeats $N_CV_REPEATS --n_cv_splits $N_CV_SPLITS \
         --drop $DROP --bias_fraction 0.1 --mrs_function $MRS_FUNCTION --load_previous_results
     done
-done
-
-for MRS_FUNCTION in mrs_step random 
-do
-    python src/mrs_analysis.py --data_set_name folktables_income --bias_type mean_difference --n_cv_repeats $N_CV_REPEATS --n_cv_splits $N_CV_SPLITS \
-    --drop $DROP --bias_fraction 0.8 --mrs_function $MRS_FUNCTION --load_previous_results
 done
--- slurm/mrs_analysis_slurm.sh
+++ slurm/mrs_analysis_slurm.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 #SBATCH -A m2_datamining
-#SBATCH -p parallel 
-#SBATCH -J "feature_weighting_cross_validation" # gives SLURM_JOB_NAME
+#SBATCH -p longtime 
+#SBATCH -J "MRS Analysis" # gives SLURM_JOB_NAME
 #SBATCH -n 1 # gives SLURM_NTASKS
-#SBATCH -t 5-00 
+#SBATCH -t 10-00 
 #SBATCH --cpus-per-task=5
-#SBATCH --nodes=1
 #SBATCH --mem=16G
-#SBATCH --array=1-12
-
+#SBATCH --array=1-4
 
 N_CV_REPEATS=10
 N_CV_SPLITS=5

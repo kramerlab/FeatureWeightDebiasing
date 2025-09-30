27aac8f40a2429191c1b49a764a21b13490c52b1
Tony Hauptmann <65155653+thauptmann@users.noreply.github.com>
2025-09-30 08:46:17 +0200

    Convert float to int for n_splits

--- src/weighting_methods/maximum_representative_subsampling.py
+++ src/weighting_methods/maximum_representative_subsampling.py
@@ -266,10 +266,10 @@ def random_drops(
     dropped_N = N[sample_weights != 0.0]
     y = dropped_N[target]
     target_sum = np.sum(y)
-    if (target_sum < n_splits):
-        n_splits = target_sum
+    if target_sum < n_splits:
+        n_splits = int(target_sum)
     elif (len(dropped_N) - target_sum) < n_splits:
-        n_splits = len(dropped_N) - target_sum
+        n_splits = int(len(dropped_N) - target_sum)
     if n_splits < 2:
         return None, None
 
